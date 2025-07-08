7d54cc005368e9b5ad810ff32ecd69251590da6a
Deathcrusher <161490744+Deathcrusher@users.noreply.github.com>
2025-07-08 20:26:08 +0200

    Use python -m pyinstaller in Windows compile script

--- compile.ps1
+++ compile.ps1
@@ -7,13 +7,13 @@ $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location $scriptDir
 
 # Install required Python packages and PyInstaller
-pip install -r requirements.txt pyinstaller
+python -m pip install -r requirements.txt pyinstaller
 
 # Build optional C extension
 python setup.py build_ext --inplace
 
-# Build Client A and Client B executables
-pyinstaller --onefile --windowed -m onionchat.client_a_main -n client_a_main
-pyinstaller --onefile --windowed -m onionchat.client_b_main -n client_b_main
+# Build Client A and Client B executables using the PyInstaller module
+python -m pyinstaller --onefile --windowed -m onionchat.client_a_main -n client_a_main
+python -m pyinstaller --onefile --windowed -m onionchat.client_b_main -n client_b_main
 
 Write-Host "Executables are available in the dist/ directory."
